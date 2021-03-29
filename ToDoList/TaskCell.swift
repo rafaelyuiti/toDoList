28af8b28cd0f63f196d3411b76160a01789035e4
rafaelyuiti <rafael_y91@hotmail.com>
2021-03-29 13:33:32 -0300

    primeiro comia

---
+++ ToDoList/TaskCell.swift
@@ -0,0 +1,18 @@
+//
+//  TaskCell.swift
+//  ToDoList
+//
+//  Created by Rafael Yuiti on 29/03/21.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBAction func checkBoxAction(_ sender: Any) {
+    }
+    @IBOutlet weak var checkBoxOutlet: UIButton!
+    @IBOutlet weak var taskNameLabel: UILabel!
+    
+
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -7,13 +7,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    var tasks: [Task] = []
+    
+    //var task1 = Task(name: "task1")
+    //var task2 = Task(name: "task2")
+    
+    
+    var items = [String]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! TaskCell
+        
+        cell.taskNameLabel?.text = tasks[indexPath.row].name
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "To Do List"
+        tasks.append(Task(name: "teste"))
+        view.addSubview(tableView)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+        tableView.reloadData()
+        print(tasks)
+        
+    }
+    
+    @objc private func didTapAdd(){
+        let alert = UIAlertController(title: "New Item", message: "Enter new to do list item!", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: { field in
+            field.placeholder = "Enter item..."
+        })
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { (_) in
+            if let field = alert.textFields?.first{
+                if let text = field.text, !text.isEmpty{
+                    
+                    //digita uma nova to do list
+                    //print(text)
+                    let newTask = Task(name: text)
+                    self.tasks.append(newTask)
+                    self.tableView.reloadData()
+                    
+                }
+            }
+        }))
+        
+        present(alert, animated: true)
     }
 
+}
 
+class Task{
+    var name = ""
+    var checked = false
+    
+    convenience init(name: String) { //inicializador
+        self.init()
+        self.name = name
+    }
 }
 
